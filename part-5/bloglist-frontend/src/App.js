d886104b4870e03d7f8a16fee76774330645de2a
Sparkvix <ev1lhat0l0ls@gmail.com>
2020-08-01 00:13:45 -0400

    Part5: Exercises 5.2: bloglist frontend, step2 complete

--- part-5/bloglist-frontend/src/App.js
+++ part-5/bloglist-frontend/src/App.js
@@ -10,6 +10,14 @@ const App = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBloglistUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
@@ -20,12 +28,19 @@ const App = () => {
       console.log("login succeed with", username, password);
       const user = await loginService.login({ username, password });
 
+      window.localStorage.setItem("loggedBloglistUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
     } catch (exception) {
       console.log("Wrong credentials");
     }
+  };
+
+  const handleLogout = () => {
+    console.log(`${user.name} log out`);
+    window.localStorage.removeItem("loggedBloglistUser");
+    setUser(null);
   };
 
   const loginForm = () => (
@@ -58,7 +73,9 @@ const App = () => {
   const blogPage = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>log out</button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
