1841091ba43e827648868f25bec79d3e800a877f
Sparkvix <ev1lhat0l0ls@gmail.com>
2020-08-06 08:02:13 -0400

    Part 6: Exercises 6.1: unicafe revisited, step1 complete

---
+++ part-6/unicafe-redux/src/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "GOOD":
+      const good = state.good + 1;
+      return { ...state, good };
+    case "OK":
+      const ok = state.ok + 1;
+      return { ...state, ok };
+    case "BAD":
+      const bad = state.bad + 1;
+      return { ...state, bad };
+    case "ZERO":
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
