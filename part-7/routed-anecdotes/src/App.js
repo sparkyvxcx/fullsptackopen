c1e38ba9e8bcb121a391fc339ebf069d109fe3d9
Sparkvix <ev1lhat0l0ls@gmail.com>
2020-08-15 06:26:48 -0400

    Part 7: Exercise 7.6: anecdotes and hooks step3 complete

--- part-7/routed-anecdotes/src/App.js
+++ part-7/routed-anecdotes/src/App.js
@@ -115,15 +115,23 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
+          />
         </div>
         <div>
           author
-          <input {...author} />
+          <input
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
+          />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input type={info.type} value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
         <button onClick={onclickHandle}>reset</button>
